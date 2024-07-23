fa18e8a0d911a17fa71299e56a050472f8c18c49
Admin <Admin@DESKTOP-OIH4DR4>
2024-07-23 10:20:35 +0530

    committed

---
+++ src/samplepractice/Factorial.java
@@ -0,0 +1,20 @@
+package samplepractice;
+
+
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int i, fact=1, num =3;
+		
+		for(i=1; i<=num;i++)
+		{
+			fact = fact*i;
+			
+		}
+		System.out.println("Factorial of "+num+" is: "+fact);
+		
+
+	}
+
+}
